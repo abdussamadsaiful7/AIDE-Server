59bb634cd5c77fa0c86b38135f113751444ac4a1
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-07-14 00:32:57 +0600

    added get method

--- index.js
+++ index.js
@@ -26,6 +26,16 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const productsCollection = client.db('aidreDB').collection('products');
+
+        app.get('/products', async(req, res)=>{
+            const cursor =  productsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
